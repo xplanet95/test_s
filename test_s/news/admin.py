1065eacfab2654a659b3c10c3e6ee62afdfaa7d5
xplanet95 <vladis.box@yandex.ru>
2022-05-18 15:04:11 +0800

    --

--- test_s/news/admin.py
+++ test_s/news/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'content', 'created_at', 'updated_at', 'is_published')
-    list_display_links = ('id', 'content')
+    list_display = ('id', 'title', 'content', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
     search_fields = ('title', 'content',)
     list_filter = ('created_at', 'updated_at')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, NewsAdmin)
--- test_s/news/models.py
+++ test_s/news/models.py
@@ -7,6 +7,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -15,3 +16,11 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
