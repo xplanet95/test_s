16cabe393ffaf3011140a8fce27eb7b9aa535d99
xplanet <vladis.box@yandex.ru>
2022-05-18 22:01:13 +0800

    --

--- test_s/news/admin.py
+++ test_s/news/admin.py
@@ -3,16 +3,16 @@ from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'content', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content',)
     list_filter = ('created_at', 'updated_at')
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'is_published')
+    list_display = ('id', 'title')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
 
 admin.site.register(News, NewsAdmin)
-admin.site.register(Category, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- test_s/news/models.py
+++ test_s/news/models.py
@@ -7,7 +7,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -21,6 +21,10 @@ class News(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['title']
